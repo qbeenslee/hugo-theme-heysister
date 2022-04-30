e59f8b1fa6635090b4f97c54468474acb5c97323
qbeenslee <qbeenslee@gmail.com>
2022-05-01 00:01:23 +0800

    Rm: remove photoswipe.js

--- static/js/main.js
+++ static/js/main.js
@@ -157,7 +157,11 @@ $(document).ready(function() {
     /*
     Initialise Photoswipe
     */
-    var items = []; // array of slide objects that will be passed to PhotoSwipe()
+    if(mediumZoom==undefined){
+        return;
+    }
+    const zoom = mediumZoom();
+    zoom.update({ background: '#000C',margin:45})
 
     var attachHolder = function() {
         // get properties from child a/img/figcaption elements,
@@ -190,59 +194,8 @@ $(document).ready(function() {
         if ($src == undefined) {
             $src = $msrc;
         }
-        // if data-size on <a> tag is set, read it and create an item
-        if ($img.data("size")) {
-            var $size = $img.data("size").split("x");
-            var item = {
-                src: $src,
-                w: $size[0],
-                h: $size[1],
-                title: $title,
-                msrc: $msrc,
-            };
-            //console.log("Using pre-defined dimensions for " + $src);
-            // if not, set temp default size then load the image to check actual size
-        } else {
-            var item = {
-                src: $src,
-                w: 800, // temp default size
-                h: 600, // temp default size
-                title: $title,
-                msrc: $msrc,
-            };
-            //console.log("Using default dimensions for " + $src);
-            // load the image to check its dimensions
-            // update the item as soon as w and h are known (check every 30ms)
-            var img = new Image();
-            img.src = $src;
-            var wait = setInterval(function() {
-                var w = img.naturalWidth,
-                    h = img.naturalHeight;
-                if (w && h) {
-                    clearInterval(wait);
-                    item.w = w;
-                    item.h = h;
-                    //console.log("Got actual dimensions for " + img.src);
-                }
-            }, 30);
-        }
-        // Save the index of this image then add it to the array
-        var index = items.length;
-        items.push(item);
-        // Event handler for click on a figure
-        $img.on("click", function(event) {
-            event.preventDefault(); // prevent the normal behaviour i.e. load the <a> hyperlink
-            // Get the PSWP element and initialise it with the desired options
-            var $pswp = $(".pswp")[0];
-            var options = {
-                index: index,
-                bgOpacity: 0.8,
-                showHideOpacity: true,
-            };
-            new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options).init();
-        });
+        zoom.attach(this);
     };
-
     // for every figure element on the page:
     $("#content img").each(attachHolder);
 });
